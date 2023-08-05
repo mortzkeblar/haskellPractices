5fb6d31d4d3a90eb562087163f81f65571aa896b
mortzkeblar <mortzkeb@gmail.com>
2023-08-05 01:48:30 -0300

    solve from step 3a

--- TestCarreras.hs
+++ TestCarreras.hs
@@ -6,10 +6,7 @@ data Auto = Auto {
 	distancia :: Int
 } deriving (Eq, Show)
 
-data Carrera = Carrera {
-	autos :: [Auto]
-}
-
+type Carrera = [Auto]
 
 rayo = Auto "rojo" 80 90
 mate = Auto "oxido" 30 20
@@ -18,7 +15,7 @@ luigi = Auto "amarillo" 50 66
 mack = Auto "rojo trailer" 40 60
 sheriff = Auto "negro" 60 70
 franchesco = Auto "rojo verde" 75 85
-
+tom = Auto "patito" 90 110
 {-
 rayo = Auto "rojo" 0 0
 mate = Auto "oxido" 0 0
@@ -28,4 +25,5 @@ mack = Auto "rojo trailer" 0 0
 sheriff = Auto "negro" 0 0
 franchesco = Auto "rojo verde" 0 0
 -}
-carreraAutos = Carrera [rayo, mate, doc, luigi, mack, sheriff, franchesco]
+carreraAutos :: Carrera
+carreraAutos = [rayo, mate, doc, luigi, mack, sheriff, franchesco, tom]
